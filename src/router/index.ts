5adc3e080d7209fd5431d4762e01722f7a6200b9
Quentin Drouet <quentindrouet@MacBook-Pro-de-Quentin-2.local>
2023-06-25 21:54:28 +0200

    minor update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import CityDetailView from '../views/CityDetailView.vue';
 
